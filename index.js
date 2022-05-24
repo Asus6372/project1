3ff6becb51eecaf5804a35fac9ffb4ad74dae122
dev-Prateek028 <86090430+dev-Prateek028@users.noreply.github.com>
2022-05-24 14:33:22 +0530

    FIRST COMMIT

---
+++ index.js
@@ -0,0 +1,78 @@
+//console.log("hello world")
+/*console.error("this is an error")
+console.warn("this is an warning")
+arr = [1, 2, "bablu", 4, 5];
+console.log(arr);
+console.log(arr[1]);
+console.log(arr[4])
+console.log(a + b)
+var a = 7;
+var b = 8;
+console.log(true && true);
+console.log(true && true);
+console.log(true && true);
+console.log(true && true);
+console.log(!true);
+function avg(a, b) {
+    c = (a + b) / 2;
+    return c;
+}
+var c1 = avg(4, 6);
+console.log(c1)
+var age = 8;
+if (age > 2) {
+    console.log("you are small");
+}
+else if (age < 12) {
+    console.log('you are tatti')
+}
+arr.forEach(function (element) {
+    console.log(element)
+
+});
+let j = 0;
+while (j < arr.length) {
+    console.log(arr[j]);
+    j++;
+}
+//undertanding the break statement
+for(var i=0;i<arr.length;i++){if(i==2){
+    break
+}
+    console.log(arr[i])
+}*/
+let myarr=["fan","reddy","harry","joy"];
+
+//console.log(myarr)
+//console.log(myarr.length)
+//myarr.pop();
+//console.log(myarr)
+//myarr.push('guru');
+//myarr.shift();
+//myarr.sort();
+//console.log(mylovelystring.indexOf("ayush"))
+//let mylovelystring = "ayush is a good boy"
+////console.log(mylovelystring.length)
+//console.log(mylovelystring.slice(1,3))
+//console.log(mylovelystring.replace('ayush','rohan'))
+//console.log(mylovelystring)
+//let mydate = new Date();
+let elem = document.getElementById('click');
+let elemclass = document.getElementsByClassName('container')
+//elemclass[0].style.background ='yellow'
+//elemclass[0].classList.add("bg-primary")
+//elemclass[0].classList.add("text-sucess")
+//console.log(elem.innerHTML)
+//console.log(elem.innerText)
+//console.log(elemclass[0].innerHTML)
+//console.log(elemclass[0].innerText)
+//tn=document.getElementsByTagName('div')
+//console.log(tN
+function clicked(){
+    console.log('the button')
+}
+firstcontainer.addeventlistener('mouseover',function(){console.log("mouse on container")})
+ 
+
+
+
